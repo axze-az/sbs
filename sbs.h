f9df19801cfd84d2c20124dcced7fb58e8e6f1b9
axel <axel@66bf8996-65f0-437a-9b16-6deb24255ddb>
2009-12-21 14:37:40 +0000

    V-0.3.14: different queue defaults
    git-svn-id: file:///usr/local/svn/sbs/trunk@107 66bf8996-65f0-437a-9b16-6deb24255ddb

--- sbs.h
+++ sbs.h
@@ -25,7 +25,7 @@
 #include <pwd.h>
 #include <signal.h>
 
-#define SBS_VERSION "simple batch system V-0.3.12"
+#define SBS_VERSION "simple batch system V-0.3.14"
 /*
  * queue layout: 
  * basedir/queue/jobs/.active.0

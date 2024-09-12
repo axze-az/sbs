6e25843f3bf4c3832f3fd01409bc7d7bc7146609
axdev <az0123456@gmx.de>
2024-09-12 17:15:35 +0200

    version 0.6.14

--- sbs.h
+++ sbs.h
@@ -25,7 +25,7 @@
 #include <pwd.h>
 #include <signal.h>
 
-#define SBS_VERSION "simple batch system V-0.6.13"
+#define SBS_VERSION "simple batch system V-0.6.14"
 /*
  * queue layout: 
  * basedir/queue/jobs/

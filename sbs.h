fe5da53ef6d9e9b10ef6cb64ebb62bb5c16db58e
axdev <az0123456@gmx.de>
2024-11-01 17:33:12 +0100

    sbs 0.6.16: correct the systemd service file

--- sbs.h
+++ sbs.h
@@ -25,7 +25,7 @@
 #include <pwd.h>
 #include <signal.h>
 
-#define SBS_VERSION "simple batch system V-0.6.15"
+#define SBS_VERSION "simple batch system V-0.6.16"
 /*
  * queue layout: 
  * basedir/queue/jobs/
